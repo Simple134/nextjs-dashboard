3ba87c99b791010cc404110c81fa0ccfe30322ca
Josue Sanchez <sanchezjosue2001@gmail.com>
2024-03-27 18:55:43 -0400

    feat: metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,12 @@ import { lusitana } from '../../ui/font';
 import {  fetchLatestInvoices, fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
 
     const latestInvoices = await fetchLatestInvoices()
